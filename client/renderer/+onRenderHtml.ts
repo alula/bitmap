1f336fd2d5fb639d63e3f4e0d61c5938a5fdd515
Alula <6276139+alula@users.noreply.github.com>
2024-09-01 11:40:30 +0200

    Fix HTML generation

--- client/renderer/+onRenderHtml.ts
+++ client/renderer/+onRenderHtml.ts
@@ -3,10 +3,5 @@ import { dangerouslySkipEscape, escapeInject } from "vike/server";
 export async function onRenderHtml(pageContext: Vike.PageContext) {
 	const { Page } = pageContext;
 	const pageHtml = Page.renderHTML();
-	return escapeInject`<!DOCTYPE html>
-    <html>
-      <body>
-        <div id="page-view">${dangerouslySkipEscape(pageHtml)}</div>
-      </body>
-    </html>`;
+	return escapeInject`${dangerouslySkipEscape(pageHtml)}`;
 }
