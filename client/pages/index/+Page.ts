94ba4f304f285cfb86a84c97b7aa6ee7eeb40e79
Alula <6276139+alula@users.noreply.github.com>
2024-09-01 12:09:54 +0200

    add og info

--- client/pages/index/+Page.ts
+++ client/pages/index/+Page.ts
@@ -9,7 +9,11 @@ function renderHTML() {
 		<meta charset="UTF-8" />
 		<!-- <link rel="icon" type="image/svg+xml" href="favicon.svg" /> -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<title>Bitmap</title>
+		<title>1 billion checkboxes</title>
+
+		<meta property="og:title" content="1 billion checkboxes" />
+		<meta property="og:description" content="1024³ (64⁵) checkboxes. 128 MiB bitmap. Come over and check all the billion boxes!" />
+		<meta property="theme-color" content="#fe640b" />
 	</head>
 	<body>
 		<div id="app"></div>
